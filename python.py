6adcf2b609556151c623b3871e4c1b6226cf6f01
kajal8321 <146552306+kajal8321@users.noreply.github.com>
2023-10-20 23:28:59 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+# write a python program that acccepts a word from the user and reversed it.
+
+Name=str(input("enter the name:"))
+temp=""
+for i in Name:
+    temp=i+temp
+print("reversed_name:" ,temp)    
